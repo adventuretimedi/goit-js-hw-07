d31df3505a86053b89bd3bf984944ee9c8e69267
Diana Peretiatko <olhadianaaa@gmail.com>
2023-02-24 18:41:44 +0200

    initial commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const createGalleryCardsMarkup = (galleryItems) => {
+  const { preview, original, description } = galleryItems;
+  return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src=${preview} data-source=${original} alt=${description} />
+    </a>
+  </div>`;
+};
+
+const gallery = document.querySelector(".gallery");
+const createGallery = galleryItems.map(createGalleryCardsMarkup).join("");
+gallery.insertAdjacentHTML("beforeend", createGallery);
+// console.log(createGallery);
+
+gallery.addEventListener("click", selectModalImg);
+function selectModalImg(e) {
+    e.preventDefault();
+    console.log(e)
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImg = e.target.dataset.source;
+//   const instance = basicLightbox.create(`
+//     <img src=${selectedImg}/>`);
+//     instance.show();
+//     console.log(selectedImg);
+
+    const instance = basicLightbox.create(`<img src=${selectedImg}/>`)
+
+instance.show()
+}
+
