d4abcb053eb771b31ed8fd848f3c81d791ea4021
Diana Peretiatko <olhadianaaa@gmail.com>
2023-02-25 20:08:53 +0200

    Gallery creation (HW7)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,9 @@
 import { galleryItems } from "./gallery-items.js";
-// console.log(galleryItems);
 
 const createGalleryCardsMarkup = (galleryItems) => {
   const { preview, original, description } = galleryItems;
   return `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+    <a class="gallery__link" href="large-image.jpg"> 
       <img class="gallery__image" src=${preview} data-source=${original} alt=${description} />
     </a>
   </div>`;
@@ -12,24 +11,35 @@ const createGalleryCardsMarkup = (galleryItems) => {
 
 const gallery = document.querySelector(".gallery");
 const createGallery = galleryItems.map(createGalleryCardsMarkup).join("");
+
 gallery.insertAdjacentHTML("beforeend", createGallery);
-// console.log(createGallery);
 
 gallery.addEventListener("click", selectModalImg);
+
 function selectModalImg(e) {
-    e.preventDefault();
-    console.log(e)
+  e.preventDefault();
+  document.addEventListener("keyup", exitFromModal);
+
   if (e.target.nodeName !== "IMG") {
     return;
   }
   const selectedImg = e.target.dataset.source;
-//   const instance = basicLightbox.create(`
-//     <img src=${selectedImg}/>`);
-//     instance.show();
-//     console.log(selectedImg);
+  //create modal
+  const instance = basicLightbox.create(
+    `                  
+  <img src=${selectedImg} alt="Gallery Image" />`,
+    {
+      onClose: () => {
+        document.removeEventListener("keyup", exitFromModal);
+      },
+    }
+  );
+  instance.show();
 
-    const instance = basicLightbox.create(`<img src=${selectedImg}/>`)
-
-instance.show()
+  function exitFromModal(e) {
+    if (e.key === "Escape") {
+      console.log(e.key);
+      instance.close();
+    }
+  }
 }
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const createGalleryCardsMarkup = (galleryItems) => {
+  const { preview, original, description } = galleryItems;
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src=${preview} alt=${description} title=${description}/>
+</a>`;
+};
+
+const gallery = document.querySelector(".gallery");
+const createGallery = galleryItems.map(createGalleryCardsMarkup).join("");
+
+gallery.insertAdjacentHTML("beforeend", createGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
